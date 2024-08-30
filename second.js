60c8d5625be54faeba370947a25ff69714b3c3e2
Carlitos013 <136674364+Carlitos013@users.noreply.github.com>
2024-08-29 22:47:09 -0600

    sa
    pri sa

---
+++ second.js
@@ -0,0 +1,10 @@
+// script.js
+// Crear un elemento de imagen
+var img = document.createElement("img");
+
+// Asignar la fuente de la imagen
+img.src = "\Users\carli\vsc\scripts\iamges.jpeg";
+
+// Añadir la imagen al contenedor
+var imageContainer = document.getElementById("imageContainer");
+imageContainer.appendChild(img);
